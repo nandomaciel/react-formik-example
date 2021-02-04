3b576eb584cac4fe2aa96a3d44915051670e418f
Fernando Rodrigues Maciel <fernandomaciel@ufrn.edu.br>
2021-02-04 15:17:31 -0300

    Formik

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Formik, Form, Field } from 'formik';
+
 
 function App() {
+
+  function onSubmit(values, actions) {
+    console.log(values)
+  }
+
+  function validate(values) {
+    const errors = {};
+
+    if(!values.name){
+      errors.name = 'Nome é obrigatório!';
+    }
+
+    if(!values.email){
+      errors.email = 'Email é obrigatório!';
+    }
+    return errors;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik
+        validate={validate}
+        onSubmit={onSubmit}
+        initialValues={{
+          name: '',
+          email: ''
+        }}
+        render={({ values, errors }) => (
+          <Form>
+            <div>
+              <label>Nome</label>
+              <Field name="name" type="text" />
+              {errors.name && (
+                <span>{errors.name}</span>
+              )}
+            </div>
+            <div>
+              <label>E-mail</label>
+              <Field name="email" type="email" />
+              {errors.email && (
+                <span>{errors.email}</span>
+              )}
+            </div>
+            <button type="submit">Enviar</button>
+          </Form>
+        )}
+      />
     </div>
   );
 }
